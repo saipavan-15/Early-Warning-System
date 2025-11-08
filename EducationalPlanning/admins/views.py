8cc8ad5fff6a6a8b8a973a6f2fcbdc77841752c5
Your Name <you@example.com>
2025-11-08 12:25:41 +0530

    Fix absolute import paths in admins/views.py

--- EducationalPlanning/admins/views.py
+++ EducationalPlanning/admins/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib import messages
-from users.models import UserRegistrationModel
-
+from EducationalPlanning.users.models import UserRegistrationModel
 
 
 def AdminLoginCheck(request):
@@ -30,25 +29,21 @@ def ViewRegisteredUsers(request):
 def AdminActivaUsers(request):
     if request.method == 'GET':
         id = request.GET.get('uid')
-        status = 'activated'
-        print("PID = ", id, status)
-        UserRegistrationModel.objects.filter(id=id).update(status=status)
-        data = UserRegistrationModel.objects.all()
-        return render(request, 'admins/RegisteredUsers.html', {'data': data})
+        UserRegistrationModel.objects.filter(id=id).update(status='activated')
+    data = UserRegistrationModel.objects.all()
+    return render(request, 'admins/RegisteredUsers.html', {'data': data})
 
 
 def DeleteUsers(request):
     if request.method == 'GET':
         id = request.GET.get('uid')
-        status = 'activated'
-        print("PID = ", id, status)
         UserRegistrationModel.objects.filter(id=id).delete()
-        data = UserRegistrationModel.objects.all()
-        return render(request, 'admins/RegisteredUsers.html', {'data': data})
+    data = UserRegistrationModel.objects.all()
+    return render(request, 'admins/RegisteredUsers.html', {'data': data})
 
 
 def adminMLResults(request):
-    from users.algorithms import ImplementAlgorithmsCodes
+    from EducationalPlanning.users.algorithms import ImplementAlgorithmsCodes
     knn = ImplementAlgorithmsCodes.knnResults()
     rf = ImplementAlgorithmsCodes.randomForest()
     svm = ImplementAlgorithmsCodes.svmAlgorithm()
