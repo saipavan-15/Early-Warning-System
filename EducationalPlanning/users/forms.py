bb6e6ba9cba5867732ce03628cea491634471c47
Your Name <you@example.com>
2025-11-08 12:19:27 +0530

    Fix regex pattern warnings

--- EducationalPlanning/users/forms.py
+++ EducationalPlanning/users/forms.py
@@ -5,12 +5,12 @@ from .models import UserRegistrationModel
 class UserRegistrationForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'pattern': '[a-zA-Z]+'}), required=True, max_length=100)
     loginid = forms.CharField(widget=forms.TextInput(attrs={'pattern': '[a-zA-Z]+'}), required=True, max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'pattern': '(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}',
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'pattern': r'(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'}))
                                                                  'title': 'Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters'}),
                                required=True, max_length=100)
     mobile = forms.CharField(widget=forms.TextInput(attrs={'pattern': '[56789][0-9]{9}'}), required=True,
                              max_length=100)
-    email = forms.CharField(widget=forms.TextInput(attrs={'pattern': '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$'}),
+    email = forms.CharField(widget=forms.TextInput(attrs={'pattern': r'[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$'}))
                             required=True, max_length=100)
     locality = forms.CharField(widget=forms.TextInput(), required=True, max_length=100)
     address = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 22}), required=True, max_length=250)
